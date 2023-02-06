139ea5d92ca8156b588d1d6985a2c1e92df04e55
wsnowy555 <123211070+wsnowy555@users.noreply.github.com>
2023-02-06 21:29:10 +0100

    wrzuta

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+var cssDest = 'style';
+var cssInputFile = 'source/style.scss';
+var watchedFiles = 'source/**/*.scss';
+
+gulp.task ('buildcss', function(){
+    return gulp.src(cssInputFile)
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest(cssDest));
+});
+gulp.task ('watch', function(){
+    gulp.watch(watchedFiles, gulp.series('buildcss'));
+});
